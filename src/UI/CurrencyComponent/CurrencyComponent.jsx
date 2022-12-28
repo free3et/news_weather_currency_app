e85caea9bc321a34d1f91fc13dbbf908f343a61a
free3et <free3et@gmail.com>
2022-12-28 12:48:27 +0200

    update structure

--- src/UI/CurrencyComponent/CurrencyComponent.jsx
+++ src/UI/CurrencyComponent/CurrencyComponent.jsx
@@ -1,18 +1,18 @@
-import { RowCurrency } from "./Row"
-import { useCurrency } from "../../context/hooks/useCurrency"
-import style from './Currency.module.scss'
+import { RowCurrency } from "./Row";
+import { useCurrency } from "../../context/hooks/useCurrency";
+import style from "./Currency.module.scss";
 export const Currency = () => {
-    const [currency, isLoading,errorMessage] = useCurrency();
-    return (
-
-        <div className={`${style.currency} col-lg-3`}>
-            <h3 className={style.title}>Курс НБУ</h3>
-            {errorMessage.error ?
-                (<p className={style.error}>{errorMessage.message}</p>
-                ) : isLoading ? <span className={style.loader}></span> : currency !== undefined ? currency.map((element, i) =>
-                    <RowCurrency element={element} key={i} />
-                ) : null}
-        </div>
-
-    )
-}
+  const [currency, isLoading, errorMessage] = useCurrency();
+  return (
+    <div className={style.currency}>
+      <h3 className={style.title}>Курс НБУ</h3>
+      {errorMessage.error ? (
+        <p className={style.error}>{errorMessage.message}</p>
+      ) : isLoading ? (
+        <span className={style.loader}></span>
+      ) : currency !== undefined ? (
+        currency.map((element, i) => <RowCurrency element={element} key={i} />)
+      ) : null}
+    </div>
+  );
+};
