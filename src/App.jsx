a79aa40283b7443db9701fadd6fb4c2a9945f6ba
Anna Fedorenko <fedorenkoanna197@gmail.com>
2022-12-28 22:05:59 +0200

    change app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { RouterProvider } from "react-router-dom";
-import "./grid.css";
-import "./App.css";
 import { GeneralLayout } from "./UI/GeneralLayout/GeneralLayout";
 import { Loader } from "./UI/Loader/Loader";
 import { router } from "./Router/router";
