79e381352f45ef01fa283003bc14043ea174f5cd
Anna Fedorenko <fedorenkoanna197@gmail.com>
2022-12-29 04:20:50 +0200

    change

--- src/UI/CurrencyComponent/CurrencyComponent.jsx
+++ src/UI/CurrencyComponent/CurrencyComponent.jsx
@@ -5,6 +5,7 @@ export const Currency = () => {
   const [currency, isLoading, errorMessage] = useCurrency();
   return (
 
+
     <div className={`${style.currency} col-lg-12 col-sm-7 col-md-5 mx-auto col-10`}>
       <h3 className={style.title}>Exchange rate</h3>
       <div className={style.main_unit}>
@@ -17,6 +18,7 @@ export const Currency = () => {
         ) : null}
       </div>
     </div>
-    
+
+
   );
 };
